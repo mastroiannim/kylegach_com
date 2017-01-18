d90fd3a3b1ab07b29d302c706746a489902f2b7a
ioxdue <ioxdue@gmail.com>
2017-01-18 20:02:27 +0100

    pulizia

--- components/CommentPrompt.js
+++ components/CommentPrompt.js
@@ -9,7 +9,7 @@ class CommentPrompt extends Component {
 
     return (
       <p className="mt-4 p-2 bgDarker-1">
-        This site doesn’t have comments, but I’d love for you to start a conversation on <a href={'https://twitter.com/intent/tweet?text=' + config.twitter + ' — re: ' + title + ', '}>Twitter</a> or in an <a href={'mailto:' + config.email + '?subject=Re:%20' + encodeURIComponent(title)}>email</a>.
+        Non è possibile commentare sul sito, se vuoi puoi avviare una conversazione su <a href={'https://twitter.com/intent/tweet?text=' + config.twitter + ' — re: ' + title + ', '}>Twitter</a> or in an <a href={'mailto:' + config.email + '?subject=Re:%20' + encodeURIComponent(title)}>email</a>.
       </p>
     )
   }
