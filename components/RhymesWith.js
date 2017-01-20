303f02a354611317eba010c8f94e58ed1fcb75a2
paolaccio <olivieri1982@gmail.com>
2017-01-20 19:31:31 +0100

    modifiche carmela

--- components/RhymesWith.js
+++ components/RhymesWith.js
@@ -5,14 +5,16 @@ class RhymesWith extends Component {
 
   render() {
     const rhyme = () => {
-      const rhymes = ['batch', 'hatch', 'latch', 'match', '’natch', 'patch', 'scratch', 'snatch', 'thatch']
+      const rhymes = ['Tra moglie e marito non mettere il batch', 'Tra moglie e marito non mettere il hatch', 'Tra moglie e marito non mettere il latch']
       const min = 1, max = rhymes.length
       const index = Math.floor(Math.random() * (max - min + 1)) + min;
       return rhymes[index - 1]
     }
 
     return (
-      <p className="fs-5 italic">Rhymes with {rhyme() || 'patch'}.</p>
+      <p className="fs-5 italic">
+        Rhymes with {rhyme() || 'Tra moglie e marito non mettere il dito'}.
+      </p>
     )
   }
 }
