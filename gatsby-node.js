f981863db5304990a5487f0358080cdd3c6f1f93
ioxdue <ioxdue@gmail.com>
2017-01-25 19:29:23 +0100

    Webpack.ContextReplacementPlugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,10 +2,10 @@ var Webpack = require("webpack");
 
 exports.modifyWebpackConfig = function(config, stage) {
  
-    // Create a plugin with the key 'webpack-define'.
-    config.plugin("context-replacement-plugin", Webpack.DefinePlugin, [
-        new Webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /it/)    
-    ]);
-    
+    // Create a plugin with the key 'context-replacement-plugin'.
+    config.plugin("context-replacement-plugin", 
+                  Webpack.ContextReplacementPlugin,
+                  [/moment[\/\\]locale$/, /it/] 
+                  );
   return config
 }
