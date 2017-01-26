96271183811b5215bd118a221418ec01a23c3240
ioxdue <ioxdue@gmail.com>
2017-01-26 19:21:21 +0100

    telefono corretto

--- components/Contatti.js
+++ components/Contatti.js
@@ -33,17 +33,17 @@ class Contatti extends Component {
         <div className='flexItem-50 w-100 ml-1 mb-4 mb-1 pr-3@sm pl-2@sm '>
           <div className='mt-0'>
               <div className='mt-0 h5'>{info}</div>
-              <div className='mt-1@sm'>
+              <div className='mt-2@sm'>
                 <img className='left mr-1 mt-1' src="indirizzo.png"/>
-                <div className='ml-1@sm mt-0 pl-2@sm'>{indirizzo}</div>
+                <div className='ml-1@sm mt-0 fs-5 pl-2@sm'>{indirizzo}</div>
               </div>
               <div className='mt-1@sm'>
                 <img className='left mr-1 mt-1' src="telefono.png"/>
-                <div className='ml-1@sm mt-0 '>{telefono}</div>
+                <div className='ml-1@sm mt-0 fs-5'>{telefono}</div>
               </div>
               <div className='mt-1@sm'>
                 <img className='left mr-1 mt-1' src="mail.png"/>
-                <div className='ml-1@sm mt-0 '><a href={'mailto:' + config.email}>{config.email}</a></div>
+                <div className='ml-1@sm mt-0 fs-5'><a href={'mailto:' + config.email}>{config.email}</a></div>
               </div>
           </div>
       </div>
